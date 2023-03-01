98bc3bdfa7e873ec73fd73ca41abfde404ba720e
Alex-Victor <alexvictoribeiro@gmail.com>
2023-03-01 19:33:16 -0300

    init commit

---
+++ src/test/java/br/com/wakanda/poc/Poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wakanda.poc.Poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
